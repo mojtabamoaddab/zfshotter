26ed50552fedb05982187a32b5aad8e5363189a6
Mojtaba Moaddab <mojtabamoaddab@proton.me>
2025-03-10 03:42:29 +0330

    fix: correct typo in license notice header

--- lib/logging.sh
+++ lib/logging.sh
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with ZFShoter. If not, see <http://www.gnu.org/licenses/>.
+# along with ZFShotter. If not, see <http://www.gnu.org/licenses/>.
 
 
 LOG_DATE_FORMAT="%Y-%m-%d %H:%M:%S"
